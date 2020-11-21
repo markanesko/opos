9387b3c2ee766f3cce147ad8b69922438f94dde1
mb <marko.beric@student.etf.unibl.org>
2020-11-21 18:20:51 +0000

    initial commit

---
+++ Assignment1.Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment1.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
