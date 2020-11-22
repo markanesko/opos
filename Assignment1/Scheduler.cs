86ca5959c3903578cb688922c76e0b94a816cec2
mb <marko.beric@student.etf.unibl.org>
2020-11-22 18:08:37 +0000

    adds _allTasks list; implements dequeue

--- Assignment1/Scheduler.cs
+++ Assignment1/Scheduler.cs
@@ -10,6 +10,19 @@ namespace Assignment1
     {
         UInt32 counter;
 
+        public enum Priority : UInt16
+        {
+            HIGH = 2,
+            NORMAL = 7,
+            LOW = 19,
+        }
+
+        private readonly int _maxDegreeOfParallelism;
+
+        private readonly List<(Task task, Priority priority)> _allTasks = new List<(Task, Priority)>();
+
+        private readonly Queue<Task> _executingTasks = new Queue<Task>();
+
         public Scheduler()
         {
 
@@ -19,7 +32,7 @@ namespace Assignment1
         {
             Console.WriteLine("windonz...");
         }
-
+        
         protected override IEnumerable<Task> GetScheduledTasks()
         {
             throw new NotImplementedException();
@@ -27,12 +40,32 @@ namespace Assignment1
 
         protected override void QueueTask(Task task)
         {
+            
+
             throw new NotImplementedException();
         }
 
         protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
         {
             throw new NotImplementedException();
+        }
+
+        protected override bool TryDequeue(Task task)
+        {
+            lock (_allTasks) return _allTasks.Remove(findTask(task));
+        }
+
+        private (Task, Priority) findTask(Task task)
+        {
+            return _allTasks.Find((x) => x.task.GetHashCode() == task.GetHashCode());
+        }
+
+        public void ScheduleTask(Task task, Priority priority, UInt32 maxDurationTime)
+        {
+
+            _allTasks.Add((task, priority));
+
+            QueueTask(task);
         }
     }
 }
