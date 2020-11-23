a49e9a5fe9cd2ace84cd442684be5042c8858c68
mb <marko.beric@student.etf.unibl.org>
2020-11-23 20:45:39 +0000

    adds scheduler.cs; works with task class; hacky callback to remove from executing tasks; priority part needs more work

--- Assignment1.Demo/Program.cs
+++ Assignment1.Demo/Program.cs
@@ -11,54 +11,91 @@ namespace Assignment1.Demo
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
-            //Scheduler a = new Scheduler(5);
+            Console.WriteLine("Hello World!");
+            Scheduler a = new Scheduler(5);
 
-            //Task ta = new Task(() => Console.WriteLine("omg windonzzz"));
-
-            //a.ScheduleTask(ta, Scheduler.Priority.HIGH, 1);
-
-            //while (a.GetNumberOfTasks() > 0)
-            //{
-            //    Task.Delay(1000).Wait();
-            //}
-
-            //Console.WriteLine("done? windonz");
+            Task ta = new Task(() => { Task.Delay(1000).Wait(); Console.WriteLine("omg windonzzz"); });
+            Task tb = new Task(() => { Task.Delay(5000).Wait(); Console.WriteLine("omg wsindonzzz"); });
+            Task tc = new Task(() => { Task.Delay(1000).Wait(); Console.WriteLine("omg wiaandonzzz"); });
 
 
-            SchedulerThreadPool lcts = new SchedulerThreadPool(2);
-            List<Task> tasks = new List<Task>();
+            a.ScheduleTask(ta, Scheduler.Priority.HIGH, 1);
+            a.ScheduleTask(tb, Scheduler.Priority.HIGH, 1);
+            a.ScheduleTask(tc, Scheduler.Priority.HIGH, 1);
 
-            // Create a TaskFactory and pass it our custom scheduler.
-            TaskFactory factory = new TaskFactory(lcts);
-            CancellationTokenSource cts = new CancellationTokenSource();
-
-            // Use our factory to run a set of tasks.
-            Object lockObj = new Object();
-            int outputItem = 0;
-
-            for (int tCtr = 0; tCtr <= 4; tCtr++)
+            while (a.GetNumberOfTasks() > 0)
             {
-                int iteration = tCtr;
-                Task t = factory.StartNew(() => {
-                    for (int i = 0; i < 1000; i++)
-                    {
-                        lock (lockObj)
-                        {
-                            Console.Write("{0} in task t-{1} on thread {2}   ",
-                                          i, iteration, Thread.CurrentThread.ManagedThreadId);
-                            outputItem++;
-                            if (outputItem % 3 == 0)
-                                Console.WriteLine();
-                        }
-                    }
-                }, cts.Token);
-                tasks.Add(t);
+                Task.Delay(1000).Wait();
             }
 
-            Task.WaitAll(tasks.ToArray());
-            cts.Dispose();
-            Console.WriteLine("\n\nSuccessful completion.");
+            
+
+            Scheduler b = new Scheduler(4);
+
+            for (int i = 0; i < 15; i++)
+            {
+                Task t = new Task(() =>
+                {
+                    Task.Delay(i * 100).Wait(); Console.WriteLine("omg windonzzz: " + i);
+                });
+                if (i % 2 == 0) 
+                {
+                    Console.WriteLine("added high");
+                    b.ScheduleTask(t, Scheduler.Priority.HIGH, 1);
+                } 
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("added normal");
+                    b.ScheduleTask(t, Scheduler.Priority.NORMAL, 1);
+                }
+                else
+                {
+                    Console.WriteLine("added low");
+                    b.ScheduleTask(t, Scheduler.Priority.LOW, 1);
+                }
+                
+            }
+
+            while (b.GetNumberOfTasks() > 0)
+            {
+                Task.Delay(1000).Wait();
+            }
+            Console.WriteLine("done? windonz");
+
+
+            //SchedulerThreadPool lcts = new SchedulerThreadPool(2);
+            //List<Task> tasks = new List<Task>();
+
+            //// Create a TaskFactory and pass it our custom scheduler.
+            //TaskFactory factory = new TaskFactory(lcts);
+            //CancellationTokenSource cts = new CancellationTokenSource();
+
+            //// Use our factory to run a set of tasks.
+            //Object lockObj = new Object();
+            //int outputItem = 0;
+
+            //for (int tCtr = 0; tCtr <= 4; tCtr++)
+            //{
+            //    int iteration = tCtr;
+            //    Task t = factory.StartNew(() => {
+            //        for (int i = 0; i < 1000; i++)
+            //        {
+            //            lock (lockObj)
+            //            {
+            //                Console.Write("{0} in task t-{1} on thread {2}   ",
+            //                              i, iteration, Thread.CurrentThread.ManagedThreadId);
+            //                outputItem++;
+            //                if (outputItem % 3 == 0)
+            //                    Console.WriteLine();
+            //            }
+            //        }
+            //    }, cts.Token);
+            //    tasks.Add(t);
+            //}
+
+            //Task.WaitAll(tasks.ToArray());
+            //cts.Dispose();
+            //Console.WriteLine("\n\nSuccessful completion.");
 
         }                     
     }
---
+++ Assignment1/Scheduler.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    public class Scheduler : TaskScheduler
+    {
+        public enum Priority : UInt16
+        {
+            HIGH = 2,
+            NORMAL = 7,
+            LOW = 19,
+        }
+
+        private int _maxTasksAllowed;
+
+        private readonly List<(Task task, Priority priority)> _queuedTasks = new List<(Task, Priority)>();
+
+        private readonly List<Task> _executingTasks = new List<Task>();
+
+        public Scheduler(int maxTasksAllowed)
+        {
+            _maxTasksAllowed = maxTasksAllowed;
+        }
+
+        public void PrintSomething()
+        {
+            Console.WriteLine("windonz...");
+        }
+
+        protected override IEnumerable<Task> GetScheduledTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void QueueTask(Task task)
+        {
+            lock (_queuedTasks)
+            {
+
+                if (_executingTasks.Count < _maxTasksAllowed)
+                {
+                    Task toRun = GetHighestPriorityTask();
+                    if (TryDequeue(toRun) && !_executingTasks.Contains(toRun))
+                    {
+                        _executingTasks.Add(toRun);
+                        PushTaskToThreadPool(toRun);
+                    }
+                }
+            }
+
+        }
+
+        protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+        {
+            return false;
+        }
+
+        protected override bool TryDequeue(Task task)
+        {
+            
+            lock (_queuedTasks) return _queuedTasks.Remove(FindTask(task));
+        }
+
+        private (Task, Priority) FindTask(Task task)
+        {
+            return _queuedTasks.Find((x) => x.task.GetHashCode() == task.GetHashCode());
+        }
+
+        private void PrettyPrintPriority(Priority pr)
+        {
+            if (pr == Priority.HIGH)
+            {
+                Console.WriteLine("HIGH");
+            }
+            else if (pr == Priority.NORMAL)
+            {
+                Console.WriteLine("NORMAL");
+            }
+            else
+            {
+                Console.WriteLine("LOW");
+            }
+        }
+    
+        private void PrintTaskPriority(Task task)
+        {
+            PrettyPrintPriority(_queuedTasks.Find((x) => x.task.GetHashCode() == task.GetHashCode()).priority);
+        }
+        private void PushTaskToThreadPool(Task task)
+        {
+            Console.WriteLine("started executing task: " + task.Id);
+            PrintTaskPriority(task);
+            task.Start();
+            task.ContinueWith(_ => {
+                lock (_executingTasks) _executingTasks.Remove(task);
+                QueueTask(task);
+            });
+        }
+        
+        private void DelteFromExecutingCallback(Task task)
+        {
+            lock (_executingTasks) _executingTasks.Remove(task);
+        }
+
+        private Task GetHighestPriorityTask()
+        {
+            lock (_queuedTasks)
+            {
+                if (_queuedTasks.FindAll((x) => x.priority == Priority.HIGH).Count != 0)
+                {
+                    return _queuedTasks.FindAll((x) => x.priority == Priority.HIGH).First().task;
+                }
+                else if (_queuedTasks.FindAll((x) => x.priority == Priority.NORMAL).Count != 0)
+                {
+                    return _queuedTasks.FindAll((x) => x.priority == Priority.NORMAL).First().task;
+                }
+                else if (_queuedTasks.FindAll((x) => x.priority == Priority.LOW).Count != 0)
+                {
+                    return _queuedTasks.FindAll((x) => x.priority == Priority.LOW).First().task;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public void ScheduleTask(Task task, Priority priority, UInt32 maxDurationTime)
+        {
+            Console.Write("prior in schedule task: ");
+            PrettyPrintPriority(priority);
+
+            lock (_queuedTasks)
+            {
+                _queuedTasks.Add((task, priority));
+            }
+
+            QueueTask(task);
+        }
+
+        public int GetNumberOfTasks()
+        {
+            return _queuedTasks.Count + _executingTasks.Count;
+        }
+    }
+}
